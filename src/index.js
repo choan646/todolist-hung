c85db08700366d0003d7316d809445819e0c21ab
Van-Hung <gogogo0646@gmail.com>
2021-07-17 13:41:53 +0700

    xóa devtool

--- src/index.js
+++ src/index.js
@@ -11,15 +11,7 @@ import thunk from "redux-thunk";
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__
-      ? window.__REDUX_DEVTOOLS_EXTENSION__()
-      : (f) => f
-  )
-);
+const store = createStore(rootReducer, compose(applyMiddleware(...middleware)));
 
 ReactDOM.render(
   <Provider store={store}>
